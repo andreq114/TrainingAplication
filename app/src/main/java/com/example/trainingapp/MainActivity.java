232bcccab18ac13bfd3975d11bed5bcad7b65aa7
andreq114 <andreq114@gmail.com>
2019-12-17 20:48:31 +0100

    Dodane wstępne activity z dniami treningowymi ( Działa dynamiczne dodawanie dni treningowych)

--- app/src/main/java/com/example/trainingapp/MainActivity.java
+++ app/src/main/java/com/example/trainingapp/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.trainingapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +29,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
@@ -51,5 +59,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addDay(View view){
+        final LinearLayout layout = (LinearLayout)findViewById(R.id.linearDays);
+        final Button button1 = new Button(this);
+        button1.setMinimumHeight(200);
+        button1.setTextSize(24);
+        
+
+
+        final EditText inputName = new EditText(this);                  //Dodawanie dni treningowych
+        inputName.setInputType(InputType.TYPE_CLASS_TEXT);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("Enter routine name:")
+                .setCancelable(false)
+                .setView(inputName)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        button1.setText(inputName.getText());
+                        layout.addView(button1);
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        alert.show();
     }
 }
