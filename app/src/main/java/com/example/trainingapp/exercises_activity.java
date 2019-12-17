aacac69e7914740530bca857b985a3cd2f309408
andreq114 <andreq114@gmail.com>
2019-12-17 21:22:05 +0100

    Dodane activity z cwiczeniami i przejście dynamiczne na nie z activity z dniami treningowymi

---
+++ app/src/main/java/com/example/trainingapp/exercises_activity.java
@@ -0,0 +1,14 @@
+package com.example.trainingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exercises_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercises_activity);
+    }
+}
