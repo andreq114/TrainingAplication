558e5c1136f74ac8990eb39c8965a1355250f0d5
andreq114 <andreq114@gmail.com>
2019-12-17 13:21:56 +0100

    Dodany algorytm do obliczania BMI i wstępne gui

--- app/src/main/java/com/example/trainingapp/BMI_Calc.java
+++ app/src/main/java/com/example/trainingapp/BMI_Calc.java
@@ -3,6 +3,11 @@ package com.example.trainingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static java.lang.Math.pow;
 
 public class BMI_Calc extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class BMI_Calc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi__calc);
+    }
+
+    public void calculateBMI(View view){
+        TextView weight = findViewById(R.id.bmiWeight);
+        TextView growth = findViewById(R.id.bmiGrowth);
+        Integer a = Integer.parseInt(weight.getText().toString());
+        Double b = Double.parseDouble(growth.getText().toString());
+        Double bmi = a/pow(b/100,2);
+        TextView result = findViewById(R.id.bmiResult);
+        result.setText(bmi.toString());
+
+
     }
 }
