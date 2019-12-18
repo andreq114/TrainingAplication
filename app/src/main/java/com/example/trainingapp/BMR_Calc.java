29673b481c206cb27a6b588f74b23361faebff79
kamil w <kamilwielgosz303@gmail.com>
2019-12-18 21:05:13 +0100

    added BMR calculater

---
+++ app/src/main/java/com/example/trainingapp/BMR_Calc.java
@@ -0,0 +1,47 @@
+package com.example.trainingapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+import static java.lang.Math.pow;
+
+public class BMR_Calc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmr_calc);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void calculateBMR(View view) {
+        TextView weightName = findViewById(R.id.bmrWeight);
+        TextView growthName = findViewById(R.id.bmrGrowth);
+        TextView ageName = findViewById(R.id.bmrAge);
+        Integer weight = Integer.parseInt(weightName.getText().toString());
+        Integer growth = Integer.parseInt(growthName.getText().toString());
+        Integer age = Integer.parseInt(ageName.getText().toString());
+        Double bmr = 9.99 * weight + 6.25 * growth - 4.92 * age + 5;
+        TextView result = findViewById(R.id.bmrResult);
+        result.setText(String.format("%.2f", bmr));
+
+    }
+}
