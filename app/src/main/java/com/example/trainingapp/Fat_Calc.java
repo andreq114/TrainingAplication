afa03df2df4c68e94b154aed728f0570217218b9
kamil w <kamilwielgosz303@gmail.com>
2020-01-13 22:19:19 +0100

    dodano algorytm do kalkulatora tkanki tluszczowej

--- app/src/main/java/com/example/trainingapp/Fat_Calc.java
+++ app/src/main/java/com/example/trainingapp/Fat_Calc.java
@@ -3,6 +3,8 @@ package com.example.trainingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Fat_Calc extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Fat_Calc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fat_calc);
+    }
+    public void calculateFat(View view) {
+        TextView weightName = findViewById(R.id.fatWeight);
+        TextView waistName = findViewById(R.id.fatWaist);
+        Integer weight = Integer.parseInt(weightName.getText().toString());
+        Integer waist = Integer.parseInt(waistName.getText().toString());
+        Double fat = (( 1.634 * waist - 0.1804 * weight - 98.42 ) / 2.2 * weight) * 100;
+        TextView result = findViewById(R.id.fatResult);
+        result.setText(String.format("%.2f", fat));
+
     }
 }
